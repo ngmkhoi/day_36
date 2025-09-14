69634a6a7d9fa300ff7b62b6cf09eaa5dc0a55e5
ng-mkhoi <khoinm@nexlesoft.com>
2025-09-15 00:06:41 +0700

    Updates

---
+++ assets/index-BJgLwdBP.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CGaQnLNS.js";const e="_title_1x2rt_2",s={title:e};function r(){return t.jsx("h2",{className:s.title,children:"Register Page"})}export{r as default};
--- assets/index-BbKMRqZa.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CoHGGpCm.js";const e="_title_1x2rt_2",o={title:e};function l(){return t.jsx("h2",{className:o.title,children:"Welcome to Home Page"})}export{l as default};
